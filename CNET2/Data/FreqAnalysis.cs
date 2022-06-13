730147336663e26bd12cb0be54cc0596f7ee064f
kubicek-skoleni <kubicek.brno@gmail.com>
2022-06-13 13:39:38 +0200

    FreqAnalysis - projekt Data, staticke metody

---
+++ CNET2/Data/FreqAnalysis.cs
@@ -0,0 +1,26 @@
+﻿namespace Data
+{
+    public static class FreqAnalysis
+    {
+        public static Dictionary<string, int> FreqAnalysisFromString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static async Task<Dictionary<string, int>> FreqAnalysisFromUrl(string url)
+        {
+            var httpClient = new HttpClient();
+
+            var content = await httpClient.GetStringAsync(url);
+
+            return FreqAnalysisFromString(content);
+        }
+
+        public static Dictionary<string, int> FreqAnalysisFromFile(string file)
+        {
+            var content = File.ReadAllText(file);
+
+            return FreqAnalysisFromString(content);
+        }
+    }
+}
