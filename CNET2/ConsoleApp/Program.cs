237b09e1ae5e2f53961edede9dbf801e154205e4
kubicek-skoleni <kubicek.brno@gmail.com>
2022-06-13 15:57:34 +0200

    LINQ zakladni metody

--- CNET2/ConsoleApp/Program.cs
+++ CNET2/ConsoleApp/Program.cs
@@ -1,14 +1,36 @@
 ﻿using Data;
 using Model;
+using System.Linq;
 
 Console.WriteLine("Hello, World!");
 
 
 int[] numbers = { 11, 2, 13, 44, -5, 6, 127, -99, 0, 256 };
 
+// WHERE - filtrace 
+//var result = numbers.Where(n => n > 0)
+//                    .Where(n => n < 99);
 
+//ORDEBY - razeni
+//var result = numbers.OrderBy(n => n);
 
+//AGREGACE Min,Max,Sum,Avg,Count
+//var number = ;
 
+//TAKE SKIP 
+//var result = numbers.TakeWhile(n => n > 0);
+
+// SELECT - transformace
+//var result = numbers.Select(n => Math.Abs(n));
+
+//Console.WriteLine(number);
+
+Console.WriteLine(string.Join(", ", result));
+
+//foreach (var item in result)
+//{
+//    Console.WriteLine(item);
+//}
 
 
 
