b56b003c9705f8afb413c938a0b7148973477ccf
kubicek-skoleni <kubicek.brno@gmail.com>
2022-06-13 11:49:34 +0200

    upravena FAResult

--- CNET2/Model/FAResult.cs
+++ CNET2/Model/FAResult.cs
@@ -5,6 +5,29 @@
     /// </summary>
     public class FAResult
     {
+        /// <summary>
+        /// Zdroj textu
+        /// </summary>
+        public string Source { get; set; }
 
+        public SourceType SourceType { get; set; }
+        
+        /// <summary>
+        /// Výsledná frekvenční analýza slov 
+        /// </summary>
+        public Dictionary<string, int> Words { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Source} {Words?.Count}";
+            
+
+        }
+    }
+
+    public enum SourceType
+    {
+        URL,
+        FILE
     }
 }
