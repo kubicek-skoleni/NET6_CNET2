520e5bfb320adf21d77a13428e3547bcc7a6fcc9
kubicek-skoleni <kubicek.brno@gmail.com>
2022-06-15 14:13:52 +0200

    Controlers Person, Contract

---
+++ CNET2/WebAPI/Controllers/ContractController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public IEnumerable<Contract> GetAll()
+        {
+            var dataset = Data.Serialization.LoadFromXML(@"C:\Users\StudentEN\source\repos\kubicek-skoleni\PersonDataset\dataset.xml");
+            return dataset.SelectMany(x => x.Contracts);
+        }
+    }
+}
---
+++ CNET2/WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+
+        [HttpGet("GetAll")]
+        public List<Person> GetPeople()
+        {
+            var dataset = Data.Serialization.LoadFromXML(@"C:\Users\StudentEN\source\repos\kubicek-skoleni\PersonDataset\dataset.xml");
+            return dataset;
+        }
+
+    }
+}
